6842f18482f2caaa5bfe694a4bc8b0ba8dd69bab
Matt Robinson <mthw.wm.robinson@gmail.com>
2020-10-04 18:49:38 -0400

    added more nova newspapers to the config

--- test_data_ingest/utils/test_config.py
+++ test_data_ingest/utils/test_config.py
@@ -1,6 +1,16 @@
 import data_ingest.utils.config as config
 
 
-def test_read_config():
+def test_news_config_is_well_formed():
     newspaper_config = config.read_config("newspaper")
-    assert isinstance(newspaper_config, dict)
+    for value in newspaper_config.values():
+        assert "id" in value
+        assert "url" in value
+        assert "states" in value
+        assert isinstance(value["states"], list)
+
+
+def test_news_ids_are_unique():
+    newspaper_config = config.read_config("newspaper")
+    ids = [value["id"] for value in newspaper_config.values()]
+    assert len(ids) == len(set(ids))
