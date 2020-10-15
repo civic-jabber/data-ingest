70c371925d38c82f822eff5e39100f52f21ca239
Matt Robinson <mthw.wm.robinson@gmail.com>
2020-10-15 06:50:02 -0400

    created class for news api calls

---
+++ test_data_ingest/external_services/test_news_api.py
@@ -0,0 +1,34 @@
+import pytest
+
+import data_ingest.external_services.news_api as news_api
+from data_ingest.utils.environ import modified_environ
+
+
+def test_session_connects_with_api_key():
+    with modified_environ(**{"NEWS_API_KEY": "fake-key"}):
+        session = news_api.NewsAPISession()
+
+
+def test_session_connects_with_api_key_kwarg():
+    session = news_api.NewsAPISession(api_key="fake-key")
+
+
+def test_session_raises_with_no_api_key():
+    with modified_environ("NEWS_API_KEY"):
+        with pytest.raises(ValueError):
+            session = news_api.NewsAPISession()
+
+
+class MockResponse:
+    def __init__(self, **kwargs):
+        self.params = kwargs.get("params", dict())
+
+
+def test_api_key_added_as_param():
+    news_api.Session.request = lambda self, method, url, *args, **kwargs: MockResponse(
+        **kwargs
+    )
+    with modified_environ(**{"NEWS_API_KEY": "fake-key"}):
+        session = news_api.NewsAPISession()
+    response = session.get("/parrots")
+    assert response.params["apiKey"] == "fake-key"
