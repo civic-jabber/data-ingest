cbd4e42c3dc79ae948b4ee4d41d550ce1c3cbacd
Matt Robinson <mthw.wm.robinson@gmail.com>
2020-10-17 12:36:10 -0400

    added postgres table for legislators

---
+++ sql/tables/legislators.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS civic_jabber.legislators (
+  id text,
+  name text,
+  given_name text,
+  family_name text,
+  gender text,
+  current_state text,
+  current_party text,
+  current_district text,
+  current_chamber text,
+  links text,
+  image text,
+  twitter text,
+  capitol_email text,
+  district_email text,
+  as_of_date timestamp
+) ;
