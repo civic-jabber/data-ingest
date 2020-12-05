d6f2c38b7a33fb296f63aa067add004386657f77
Matt Robinson <mthw.wm.robinson@gmail.com>
2020-12-05 16:13:39 -0500

    added test for reg to xml

--- test_data_ingest/models/test_regulation.py
+++ test_data_ingest/models/test_regulation.py
@@ -20,7 +20,7 @@ MOCK_REGULATION = {
     "summary": "This is such a great reg",
     "preamble": "This is such a great reg",
     "titles": [{"title": "VA-001", "description": "Fish"}],
-    "contacts": Contact.from_dict({"first_name": "Jabber", "last_name": "Robinson"}),
+    "contacts": [Contact.from_dict({"first_name": "Jabber", "last_name": "Robinson"})],
     "authority": "Parrot law, subsection 4",
     "effective_date": datetime.datetime(2020, 6, 8),
     "register_date": datetime.datetime(2020, 7, 20),
@@ -30,6 +30,43 @@ MOCK_REGULATION = {
 }
 
 
+REGULATION_XML = """
+<regulation>
+  <state>VA</state>
+  <issue>14</issue>
+  <volume>19</volume>
+  <notice>Notice: this is a great reg!</notice>
+  <effectiveDate>2020-06-08</effectiveDate>
+  <link>https://thebestregs.com</link>
+  <status>Final Regulation</status>
+  <chapter>Chapter 4</chapter>
+  <chapterDescription></chapterDescription>
+  <title>14-1831</title>
+<titles>
+  <title>VA-001: Fish</title>
+</titles>
+  <statuatoryAuthority>Parrot law, subsection 4</statuatoryAuthority>
+  <description>The best reg ever!</description>
+  <preamble>This is such a great reg</preamble>
+  <summary>This is such a great reg</summary>
+  <body>
+    You really don't want to read this whole thing ...
+  </body>
+<contacts>
+  <contact>
+<firstName>Jabber</firstName>
+<lastName>Robinson</lastName>
+  </contact>
+</contacts>
+</regulation>
+""".strip()
+
+
 def test_regulation_loads_from_dict():
     regulation = Regulation.from_dict(MOCK_REGULATION)
     assert regulation.to_dict() == MOCK_REGULATION
+
+
+def test_regulation_builds_xml():
+    regulation = Regulation.from_dict(MOCK_REGULATION)
+    assert regulation.xml_template() == REGULATION_XML
