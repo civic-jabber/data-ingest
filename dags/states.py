d61a262186cbb605f40d45ffa95df2c3e59722cc
Matt Robinson <mthw.wm.robinson@gmail.com>
2020-12-19 14:41:55 -0500

    start dag one day back

--- dags/states.py
+++ dags/states.py
@@ -9,7 +9,7 @@ from civic_jabber_ingest.regs.va import load_va_regulations
 default_args = {
     "owner": "civic-jabber",
     "depends_on_past": False,
-    "start_date": datetime.today(),
+    "start_date": datetime.today() - timedelta(days=1),
     "retries": 1,
     "retry_delay": timedelta(minutes=5),
 }
