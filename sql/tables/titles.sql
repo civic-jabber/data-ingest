6da105d906f7d6f5f320b9b93a8030ec0f055612
Matt Robinson <mthw.wm.robinson@gmail.com>
2020-12-22 20:21:28 -0500

    sql for titles table

---
+++ sql/tables/titles.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS civic_jabber.titles (
+  id text,
+  state text,
+  issue text,
+  volume text,
+  title text,
+  description text,
+  status text,
+  link text,
+  date timestamp,
+  start_date timestamp,
+  end_date timestamp,
+  register_date timestamp,
+  as_of_date timestamp
+) ;
